347ddb842e848adbb8ce2f952bcf60dfb5c331f1
Ryan <ryanla94@gmail.com>
2025-08-27 17:15:03 +0100

    Implemented authentication controllers and models

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { assignRequestTime, logger } = require("./middleware/logging")
 const accountRoutes = require("./routes/accounts")
 const authRoutes = require("./routes/auth")
+const { sequelize } = require("./config/mysql")
 
 const assetRoutes = require("./routes/assets")
 const portfolioRoutes = require("./routes/portfolio")
@@ -19,6 +20,16 @@ app.use("/portfolio", portfolioRoutes)
 app.use(express.json())
 app.use(assignRequestTime)
 app.use(logger)
+
+// Database
+sequelize
+    .sync()
+    .then(() => {
+        console.log("Database synced");
+    })
+    .catch((err) => {
+        console.error("Failed to sync database: ", err)
+    })
 
 app.get("/", (req, res) => res.json({message: "API is running"}));
 
---
+++ backend/controllers/authController.js
@@ -0,0 +1,111 @@
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../config")
+const User = require("../models/userModel")
+const bcrypt = require("bcrypt");
+const { response } = require("express");
+
+const createJWT = (id, name) => {
+    const payload = {
+        id: id,
+        name: name
+    }
+
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+    return token;
+}
+
+const encodePassword = (password) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            console.error("Error generating salt")
+            return
+        }
+
+        bcrypt.hash(password, salt, (err, hash) => {
+            if (err) {
+                console.error("Error hashing password")
+                return
+            };
+
+            return hash
+        })
+    });
+
+}
+
+const passwordsMatch = (inputPassword, hashedPassword) => {
+    bcrypt.compare(inputPassword, hashedPassword, (err, result) => {
+        if (err) {
+            console.error("Error comparing passwords: ", err);
+            return
+        }
+
+        if (result) {
+            return true;
+        } else {
+            return false
+        }
+    })
+}
+
+const logInUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    // Find User Logic
+    try {
+        const user = await User.findAll({
+            where: {
+                email: email
+            }
+        })
+
+        const match = passwordsMatch(password, user.password_hash)
+
+        if (!match) {
+            res.status(401).json({
+                error: "Invalid credentials"
+            })
+        }
+
+        const token = createJWT(user.id, user.username)
+
+        res.status(201).json({
+            message: "Success"
+        })
+    } catch (err) {
+        console.error("Error logging in user: ", err)
+    }
+    
+
+}
+
+const registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Create User logic
+    try {
+        const newUser = await User.create({
+            username: username,
+            email: email,
+            password_hash: encodePassword(password)
+        })
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({
+            error: "Failed to create user"
+        })
+    }
+
+
+    res.status(201).json({
+        username: newUser.username,
+        email: newUser.email
+    })
+
+    const token = createJWT(user.id, user.username)
+
+    return res.status(201).json({
+        message: "Successfully created"
+    })
+}
