78a338e5f8a89490a0e16be400fbce6bfedaaf16
Ryan <ryanla94@gmail.com>
2025-08-27 09:59:44 +0100

    Created auth routes, imported into app.js, set up login and register endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { assignRequestTime, logger } = require("./middleware/logging")
 const accountRoutes = require("./routes/accounts")
+const authRoutes = require("./routes/auth")
 
 const app = express();
 
 // Routers
 app.use("/accounts", accountRoutes)
+app.use("/auth", authRoutes)
 
+//Middleware
 app.use(express.json())
-
 app.use(assignRequestTime)
-
 app.use(logger)
 
 app.get("/", (req, res) => res.json({message: "API is running"}));
